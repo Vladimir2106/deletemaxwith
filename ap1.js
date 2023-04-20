8545a5e12c4bb26f0f664779b96e181830c32097
Vladimir <107151383+Vladimir2106@users.noreply.github.com>
2023-04-20 21:38:13 +0300

    Update ap1.js

--- ap1.js
+++ ap1.js
@@ -1,44 +1,3 @@
-/*начало бургер меню*/
-
-function m1(){
-    const d1=document.querySelector('.menu-burger')
-    const d2=document.querySelector('.f4')
-    const d3=document.querySelector('.kk121')
-    const d4=document.querySelector('.f4').querySelectorAll('ul>li')
-    
-
-    d1.addEventListener('click', function(){
-        d1.style.display="none"
-
-        d2.classList.add('f41')
-        d2.classList.remove('f4')
-
-        d3.style.display="block"
-
-for(let i of d4){
-        i.style.display="block";
-     }
-
-
-    d3.addEventListener('click',function(){
-            d1.style.display="flex"
-           
-        d2.classList.add('f4')
-        d2.classList.remove('f41')
-
-            d3.style.display="none"
-
-  for(let i of d4){
-        i.style.display="none";
-     }
-   
-    
-           })
- })
-}
-
-m1();
-
 
 
 var wii;
@@ -54,57 +13,7 @@ if(wii>768){
 }
 }
 unitt();
-    window.addEventListener('resize',unitt);/* изменение изображение масштабирование при загрузке страницы 
-unitt();
+    window.addEventListener('resize',unitt)
+unitt
 
 
-
-
-/*конец бургер меню*/
-
-
-
-/*страница 1 */
-
-/*кнопка появления + другие елементы 
-function f1(){
-const z1=document.querySelector('.kryg')
-const z2=document.querySelector('.menu')
-const z3=document.querySelector('.menu2')
-const z4=document.querySelector('.box102')
-z1.addEventListener('click', function(){
-
-    z2.classList.add('a')
-    z4.classList.add('a')
-    z1.classList.add('b')
-    z3.classList.add('b')
-    
-    setTimeout(function a2(){
-        const d3=document.querySelectorAll('.cont1')
-        for(let i of d3){
-            i.classList.remove('cont1')
-            i.classList.add('act15')
-        }  
-    },1000)
-})
-}
-f1();*/
-
-
- /* пульсация кнопки 
- const f10=document.querySelector('.im')
- console.log(f10)
- 
- function f2(){
- setInterval(function(){
-     f10.classList.add('bolhe')
-     f10.classList.remove('menhe')
- },2000)
-     
- setInterval(function(){
-     f10.classList.remove('bolhe')
-     f10.classList.add('menhe')
- },4000)
- }
- f2();*/
- /* конец поиск кнопки */
